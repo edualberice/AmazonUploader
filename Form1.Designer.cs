74eb15a4191e5f16603c56935a95e8508a8faaae
Eduardo Alberice <eduardo@odtsolucoes.com>
2014-11-14 19:27:41 -0200

    Initial commit with contributors

---
+++ Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace ODTGed_Uploader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.sendFileProgress = new System.Windows.Forms.ProgressBar();
+            this.sendingPanel = new System.Windows.Forms.Panel();
+            this.sendFileLabel = new System.Windows.Forms.Label();
+            this.startSending = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.stopSending = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.username = new System.Windows.Forms.TextBox();
+            this.password = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.loginErrorLabel = new System.Windows.Forms.Label();
+            this.sendingPanel.SuspendLayout();
+            this.startSending.SuspendLayout();
+            this.stopSending.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(11, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Enviar Arquivos";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // sendFileProgress
+            // 
+            this.sendFileProgress.Location = new System.Drawing.Point(11, 20);
+            this.sendFileProgress.Name = "sendFileProgress";
+            this.sendFileProgress.Size = new System.Drawing.Size(260, 23);
+            this.sendFileProgress.TabIndex = 1;
+            // 
+            // sendingPanel
+            // 
+            this.sendingPanel.Controls.Add(this.sendFileLabel);
+            this.sendingPanel.Controls.Add(this.sendFileProgress);
+            this.sendingPanel.Location = new System.Drawing.Point(1, 70);
+            this.sendingPanel.Name = "sendingPanel";
+            this.sendingPanel.Size = new System.Drawing.Size(283, 46);
+            this.sendingPanel.TabIndex = 2;
+            this.sendingPanel.Visible = false;
+            // 
+            // sendFileLabel
+            // 
+            this.sendFileLabel.AutoSize = true;
+            this.sendFileLabel.Location = new System.Drawing.Point(11, 4);
+            this.sendFileLabel.Name = "sendFileLabel";
+            this.sendFileLabel.Size = new System.Drawing.Size(35, 13);
+            this.sendFileLabel.TabIndex = 2;
+            this.sendFileLabel.Text = "label1";
+            // 
+            // startSending
+            // 
+            this.startSending.Controls.Add(this.button2);
+            this.startSending.Controls.Add(this.button1);
+            this.startSending.Location = new System.Drawing.Point(1, 119);
+            this.startSending.Name = "startSending";
+            this.startSending.Size = new System.Drawing.Size(283, 30);
+            this.startSending.TabIndex = 3;
+            this.startSending.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(143, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(128, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Configurações";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // stopSending
+            // 
+            this.stopSending.Controls.Add(this.button3);
+            this.stopSending.Location = new System.Drawing.Point(1, 119);
+            this.stopSending.Name = "stopSending";
+            this.stopSending.Size = new System.Drawing.Size(283, 30);
+            this.stopSending.TabIndex = 1;
+            this.stopSending.Visible = false;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(11, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(260, 23);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "Interromper Envio";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.loginErrorLabel);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.password);
+            this.panel1.Controls.Add(this.username);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(1, 52);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(283, 97);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Usuário:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Senha:";
+            // 
+            // username
+            // 
+            this.username.Location = new System.Drawing.Point(68, 4);
+            this.username.Name = "username";
+            this.username.Size = new System.Drawing.Size(202, 20);
+            this.username.TabIndex = 2;
+            // 
+            // password
+            // 
+            this.password.Location = new System.Drawing.Point(68, 30);
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(202, 20);
+            this.password.TabIndex = 3;
+            this.password.UseSystemPasswordChar = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(105, 53);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Acessar";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // loginErrorLabel
+            // 
+            this.loginErrorLabel.AutoSize = true;
+            this.loginErrorLabel.Location = new System.Drawing.Point(43, 80);
+            this.loginErrorLabel.Name = "loginErrorLabel";
+            this.loginErrorLabel.Size = new System.Drawing.Size(208, 13);
+            this.loginErrorLabel.TabIndex = 5;
+            this.loginErrorLabel.Text = "Erro ao logar. Usuário ou senha incorretos.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(285, 154);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.sendingPanel);
+            this.Controls.Add(this.startSending);
+            this.Controls.Add(this.stopSending);
+            this.Name = "Form1";
+            this.Text = "ODTDrive Uploader";
+            this.sendingPanel.ResumeLayout(false);
+            this.sendingPanel.PerformLayout();
+            this.startSending.ResumeLayout(false);
+            this.stopSending.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar sendFileProgress;
+        private System.Windows.Forms.Panel sendingPanel;
+        private System.Windows.Forms.Label sendFileLabel;
+        private System.Windows.Forms.Panel startSending;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel stopSending;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox password;
+        private System.Windows.Forms.TextBox username;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label loginErrorLabel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.ComponentModel;
+using Amazon;
+using Amazon.S3;
+using Amazon.S3.Transfer;
+
+namespace ODTGed_Uploader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            loginErrorLabel.Text = "";
+        }
+
+        private bool keepAlive = true;
+        private List<string> files = new List<string>();
+        private int totalProgress = 0;
+        private Thread sendFiles = null;
+        private Thread loginProcess = null;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.sendFiles = new Thread(new ThreadStart(sendFilesToCloud));
+            this.sendFiles.IsBackground = true;
+            this.sendFiles.Start();
+        }
+
+        private void sendFilesToCloud()
+        {
+            this.keepAlive = true;
+
+            if(sendingPanel.InvokeRequired)
+            {
+                sendingPanel.Invoke(new MethodInvoker(delegate { sendingPanel.Visible = true; }));
+            }
+
+            while (keepAlive)
+            {
+                this.files = new List<string>();
+
+                if (sendFileLabel.InvokeRequired)
+                {
+                    sendFileLabel.Invoke(new MethodInvoker(delegate { sendFileLabel.Text = "Aguardando arquivos para enviar para a nuvem"; }));
+                }
+
+                if (sendFileProgress.InvokeRequired)
+                {
+                    sendFileProgress.Invoke(new MethodInvoker(delegate { sendFileProgress.Value = 0; }));
+                }
+
+                if(startSending.InvokeRequired)
+                {
+                    startSending.Invoke(new MethodInvoker(delegate { startSending.Visible = false; }));
+                }
+
+                if(stopSending.InvokeRequired)
+                {
+                    stopSending.Invoke(new MethodInvoker(delegate { stopSending.Visible = true; }));
+                }
+
+                this.getAllFilesFromFolder();
+                if(this.files.Count > 0)
+                    this.sendFilesToBucket();
+
+                //***O tempo de espera deve vir do banco de dados
+                System.Threading.Thread.Sleep(5000);
+            }
+
+            if(sendingPanel.InvokeRequired)
+            {
+                sendingPanel.Invoke(new MethodInvoker(delegate { sendingPanel.Visible = false; }));
+            }
+
+            if(stopSending.InvokeRequired)
+            {
+                stopSending.Invoke(new MethodInvoker(delegate { stopSending.Visible = false; }));
+            }
+
+            if(startSending.InvokeRequired)
+            {
+                startSending.Invoke(new MethodInvoker(delegate { startSending.Visible = true; }));
+            }
+
+            this.sendFiles.Join();
+        }
+
+        private void getAllFilesFromFolder()
+        {
+            //*** O caminho da pasta deve vir do banco de dados
+            string[] filePaths = Directory.GetFiles("C:\\Teste");
+
+            foreach(string file in filePaths)
+            {
+                this.files.Add(file);
+            }
+        }
+
+        private void sendFilesToBucket()
+        {
+            //*** O nome do balde deve vir do banco de dados, a key também, pois a pasta será pré-definida
+            string bucket = "odtbucket";
+            string key = "";
+
+            decimal progress = 0;
+            decimal filesSent = 0;
+            decimal totalFiles = (decimal)files.Count;
+
+            int currentProgress = 0;
+
+            if (button3.InvokeRequired)
+            {
+                button3.Invoke(new MethodInvoker(delegate { button3.Enabled = false; }));
+            }
+
+            foreach(String file in this.files)
+            {
+                if (sendFileLabel.InvokeRequired)
+                {
+                    sendFileLabel.Invoke(new MethodInvoker(delegate { sendFileLabel.Text = "Enviando arquivo "+(filesSent+1)+" de "+totalFiles; }));
+                }
+
+                TransferUtility fileTransferUtility = new TransferUtility(new AmazonS3Client(Amazon.RegionEndpoint.SAEast1));
+
+                try
+                {
+                    TransferUtilityUploadRequest fileTransferUtilityRequest = new TransferUtilityUploadRequest
+                    {
+                        //*** O nome do balde vem do banco de dados
+                        BucketName = bucket,
+                        FilePath = file,
+                        StorageClass = S3StorageClass.Standard,
+                        //*** A key vem do banco de dados
+                        Key = key,
+                        CannedACL = S3CannedACL.Private,
+                        //*** Deve ser definido se haverá encriptação de acordo com o banco de dados
+                        ServerSideEncryptionMethod = ServerSideEncryptionMethod.AES256
+                    };
+                    fileTransferUtility.Upload(fileTransferUtilityRequest);
+
+                    filesSent++;
+                    progress = filesSent / totalFiles * 100;
+                    currentProgress = Convert.ToInt32(progress);
+
+                    if(sendFileProgress.InvokeRequired)
+                    {
+                        sendFileProgress.Invoke(new MethodInvoker(delegate{sendFileProgress.Value = currentProgress;}));
+                    }
+                }
+                catch (Exception exception)
+                {
+                    //Something went wrong
+                }
+
+                try
+                {
+                    string[] filePath = file.Split('\\');
+                    int last = filePath.Length - 1;
+
+                    //*** FROM deve vir do banco de dados
+                    string from = @"C:\Teste\" + filePath[last];
+                    //*** TO deve vir do banco de dados
+                    string to = @"C:\Teste\Processados\" + filePath[last];
+                    
+                    File.Move(from, to);
+                } 
+                catch(Exception e)
+                {
+                    //Could not move the files
+                }
+            }
+
+            if(sendFileLabel.InvokeRequired)
+            {
+                sendFileLabel.Invoke(new MethodInvoker(delegate { sendFileLabel.Text = "Envio realizado com sucesso"; }));
+            }
+
+            if(button3.InvokeRequired)
+            {
+                button3.Invoke(new MethodInvoker(delegate { button3.Enabled = true; }));
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Configurações
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.keepAlive = false;
+        }
+
+        private void proccessLogin()
+        {
+            string user = username.Text;
+            string pass = password.Text;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.loginProcess = new Thread(new ThreadStart(proccessLogin));
+        }
+    }
+}
